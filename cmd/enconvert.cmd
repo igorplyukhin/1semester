d72ea0bbe20270fe94319ac1d6c9c6bb3daed00e
Igor <51264207+HubbGitter@users.noreply.github.com>
2019-09-27 16:39:04 +0500

    enconvert

--- cmd/enconvert.cmd
+++ cmd/enconvert.cmd
@@ -1,25 +1,25 @@
 @echo off
+setlocal
 if "%1"=="/?" (
     echo Program converts cp886 file into utf8 in specified directory
-    echo Type directory as 1 arg
+    echo Type directory
     exit /b
 )
-
 if "%1"=="" (
     echo Type directory
     exit /b
 )
 set directory=%1
 set tmpname="tmp"
-for /r %directory% %%f in (*.txt) do (
-    
-    if exist "%directory%\%tmpname%.txt" (  
-         set tmpname=%random%
-         echo %tmpname%
+if exist "%~d0\%~p0\%tmpname%.txt" (  ::~d0 current disk ~p0 ctalog without disk and executing file
+         echo tmpfile exists!!
+         exit /b
     )
+for /r %directory% %%f in (*.txt) do (
     type %%f> %tmpname%.txt
-    cmd /U /C type %tmpname%.txt> %%f
-    del %tmpname%.txt
+    cmd /u /c type %tmpname%.txt> %%f
     echo %%f was converted
 )
-
+echo success
+del %tmpname%.txt
+endlocal
--- cmd/script.cmd
+++ cmd/script.cmd
@@ -1,15 +1,2 @@
 @echo off
-setlocal
-if "%1"=="/?" (
-    echo Program says hello
-    echo.
-    echo %0 [username]
-    exit /b
-)
-if "%1"=="" (
-set /p username=who are you?
-) else (
-    set username=%1
-)
-echo hello, %username%
-endlocal
+echo %~p0

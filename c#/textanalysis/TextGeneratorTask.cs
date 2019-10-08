cd7343a837f8a26447429b900244f6e482e496e9
Igor <51264207+DivideAndRule@users.noreply.github.com>
2019-10-08 17:04:06 +0500

    11

--- c#/textanalysis/TextGeneratorTask.cs
+++ c#/textanalysis/TextGeneratorTask.cs
@@ -9,6 +9,21 @@ namespace TextAnalysis
             string phraseBeginning,
             int wordsCount)
         {
+            while (wordsCount > 0)
+            {
+                var splitedPhrase = phraseBeginning.Split();
+                if (splitedPhrase.Length >= 2)
+                {
+                    var preLastWord = splitedPhrase[phraseBeginning.Split().Length - 2];
+                    if (nextWords.ContainsKey(preLastWord + ' ' + splitedPhrase[splitedPhrase.Length - 1]))
+                        phraseBeginning += nextWords[preLastWord + ' ' + splitedPhrase[splitedPhrase.Length -1]];
+                }
+                else if (nextWords.ContainsKey(splitedPhrase[splitedPhrase.Length - 1]))
+                    phraseBeginning += nextWords[splitedPhrase[splitedPhrase.Length - 1]];
+                else
+                    break;
+                wordsCount--;
+            }
             return phraseBeginning;
         }
     }

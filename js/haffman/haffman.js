f820c488fa9d11e8d80383d53b17b145a616db9e
Igor <51264207+DivideAndRule@users.noreply.github.com>
2019-10-09 20:42:10 +0500

    haffman

--- js/haffman/haffman.js
+++ js/haffman/haffman.js
@@ -1,49 +1,85 @@
-const fs = require("fs");
-import{Queue} from './node_c:/Users/Igor/Desktop/repos/js/haffman/node_modules/js-queue/queue.jsmodules/js-queue/'
-function CreateNode(weight, name) {
-    return {
-        code: undefined,
-        weight: weight,
-        name: name,
-        parent: undefined,
-        used: undefined
+const fs = require('fs')
+
+function Code(s, tableFile, outFile) {
+    let Node = function (count) {
+        this.count = count;
+    };
+
+    function FindCode(key, table) {
+        let code = "";
+        while (table[key].parent != undefined) {
+            code = table[key].code + code
+            key = table[key].parent;
+        }
+        return code;
+    }
+
+    function UpdateTemp(temp, min1Letter, min2Letter, min1, min2) {
+        delete temp[min1Letter];
+        delete temp[min2Letter];
+        temp[min1Letter + min2Letter] = new Node(min1 + min2);
+        table[min1Letter].parent = min1Letter + min2Letter;
+        table[min2Letter].parent = min1Letter + min2Letter;
+        table[min1Letter].code = 0;
+        table[min2Letter].code = 1;
+        table[min1Letter + min2Letter] = new Node(min1 + min2);
+    }
+
+    fs.writeFileSync(tableFile, "");
+    fs.writeFileSync(outFile, "");
+    let letters = {};
+    //Составляем словарь (буква : ее обьект)
+    for (let i = 0; i < s.length; i++)
+        if (letters[s[i]])
+            letters[s[i]].count += 1;
+        else
+            letters[s[i]] = new Node(1);
+
+    //Дублируем словарь
+    let table = { ...letters };
+    let temp = { ...letters };
+    //Строим дерево
+    while (Object.keys(temp).length > 1) {
+        let min1Letter;
+        let min2Letter;
+        let min1 = Infinity;
+        let min2 = Infinity;
+        for (let key in temp) {
+            if (temp[key].count <= min1) {
+                min2Letter = min1Letter;
+                min1Letter = key;
+                min2 = min1;
+                min1 = temp[key].count;
+            }
+        }
+        UpdateTemp(temp, min1Letter, min2Letter, min1, min2)
+    }
+    //Составляем коды
+    for (let key in table) {
+        if (key.length === 1) {
+            fs.appendFileSync(tableFile, key + " " + FindCode(key, table) + "\n");
+            table[key].code = FindCode(key, table);
+        }
+    }
+    //кодирование в файл
+    for (let i = 0; i < s.length; i++) {
+        fs.appendFileSync(outFile, table[s[i]].code);
     }
 }
 
-function UpdateNodes(node1, node2, ParentNode) {
-    node1.parent = ParentNode;
-    node2.parent = ParentNode;
-    node1.code = 0;
-    node2.code = 1;
+function Decode(s, tableFile, outFile) {
+    let strtable = fs.readFileSync(tableFile, "utf8");
+    let table = {};
+
+    for (let i = 0; i < strtable.split(/[\n ]+/).length - 1; i + 1) {
+        //table[strtable[i]] = strtable[i + 1];
+    }
+    console.log(table);
 }
 
-function ConnectNodes(node1, node2) {
-    let ParentNode = CreateNode(
-        node1.weight + node2.weight,
-        node1.name + node2.name,
-    );
-    UpdateNodes(node1, node2, ParentNode);
-    return ParentNode;
-}
-
-let letterCount = new Map();
-let letters = new Array();
 let inFile = process.argv[3];
-if (!fs.existsSync(inFile)) {
-    console.log("Input file doesn't exist");
-    return
-}
-//let outFile = fs.readFileSync(process.argv[4], "utf8")
-
+let tableFile = process.argv[4];
+let outFile = process.argv[5];
 let s = fs.readFileSync(inFile, "utf8");
-for (let i = 0; i < s.length; i++) {
-    if (letterCount.has(s[i])) {
-        letterCount.set(s[i], CreateNode(letterCount.get(s[i]).weight + 1, s[i]));
-    }
-    else {
-        letterCount.set(s[i], CreateNode(1, s[i]));
-    }
-}
 
-
-
+Decode(s, tableFile, outFile);

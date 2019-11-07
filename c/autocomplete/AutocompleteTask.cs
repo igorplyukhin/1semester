b819287504a192954949ff6422b58c4ed9b6e5e8
Igor Plyukhin <51264207+DivideAndRule@users.noreply.github.com>
2019-11-07 22:26:06 +0500

    manipulator

--- c/autocomplete/AutocompleteTask.cs
+++ c/autocomplete/AutocompleteTask.cs
@@ -13,22 +13,22 @@ namespace Autocomplete
             var index = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) + 1;
             if (index < phrases.Count && phrases[index].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
                 return phrases[index];
-            
+
             return null;
         }
-        
+
         public static string[] GetTopByPrefix(IReadOnlyList<string> phrases, string prefix, int count)
         {
-			var leftIndex = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count);
-			var rightIndex = RightBorderTask.GetRightBorderIndex(phrases, prefix, -1, phrases.Count);
-			count = Math.Min(phrases.Count - 1 - leftIndex, Math.Min(count, rightIndex - leftIndex - 1));
+            var leftIndex = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count);
+            var rightIndex = RightBorderTask.GetRightBorderIndex(phrases, prefix, -1, phrases.Count);
+            count = Math.Min(phrases.Count - 1 - leftIndex, Math.Min(count, rightIndex - leftIndex - 1));
             var topByPrefix = new string[count];
             for (var i = 0; i < count; i++)
                 topByPrefix[i] = phrases[leftIndex + i + 1];
 
             return topByPrefix;
         }
-        
+
         public static int GetCountByPrefix(IReadOnlyList<string> phrases, string prefix)
         {
             var leftBoarder = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count);
@@ -40,20 +40,41 @@ namespace Autocomplete
     [TestFixture]
     public class AutocompleteTests
     {
-        [TestCase(new []{"a", "ab", "abc"}, "a", 3, new []{"a", "ab", "abc"})]
-        [TestCase(new []{"a", "ab", "abc"}, "a", 1, new []{"a"})]
-        [TestCase(new []{"a", "ab", "abc"}, "a", 0, new string[]{})]
-        public void UniversalTopByPrefixTest(string[] nonParsedPhrases, string prefix, int count, string[] expectedTopWords)
+        [TestCase(new[] {"a", "ab", "abc"}, "a", 3, new[] {"a", "ab", "abc"})]
+        [TestCase(new[] {"a", "ab", "abc"}, "a", 100, new[] {"a", "ab", "abc"})]
+        [TestCase(new[] {"a", "ab", "abc"}, "a", 1, new[] {"a"})]
+        [TestCase(new[] {"a", "ab", "abc"}, "a", 0, new string[] { })]
+        [TestCase(new[] {"a", "ab", "abc"}, "c", 1, new string[] { })]
+        [TestCase(new[] {"a", "ab", "abc"}, "ab", 2, new[] {"ab", "abc"})]
+        [TestCase(new[] {"a", "ab", "abc"}, "", 3, new[] {"a", "ab", "abc"})]
+        [TestCase(new string[] { }, "z", 1, new string[] { })]
+        [TestCase(new[] {"a", "ab", "abc", "xx", "zz"}, "ab", 2, new[] {"ab", "abc"})]
+        public void TopByPrefixUniversalTest(string[] nonParsedPhrases,
+            string prefix, int count, string[] expectedTopWords)
         {
             var phrases = nonParsedPhrases.ToList().AsReadOnly();
             var actualTopWords = AutocompleteTask.GetTopByPrefix(phrases, prefix, count);
             Assert.AreEqual(expectedTopWords, actualTopWords);
         }
-        
+
+        [TestCase(new[] {"a"}, "a", 1)]
+        [TestCase(new[] {"aa", "ab", "bc", "bd", "be", "ca", "cb"}, "", 7)]
+        [TestCase(new[] {"aa", "ab", "bc", "bd", "be", "ca", "cb"}, "az", 0)]
+        [TestCase(new[] {"aa", "ab", "bc", "bd", "be", "ca", "cb"}, "z", 0)]
+        [TestCase(new[] {"aa", "ab", "bc", "bd", "be", "ca", "cb"}, "aa", 1)]
+        [TestCase(new[] {"aa", "ab", "bc", "bd", "be", "ca", "cb"}, "b", 3)]
+        public void CountByPrefixUniversalTest(IReadOnlyList<string> nonPaArsedPhrases, string prefix,
+            int expectedCount)
+        {
+            var phrases = nonPaArsedPhrases.ToList().AsReadOnly();
+            var actualCount = AutocompleteTask.GetCountByPrefix(phrases, prefix);
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
         [Test]
         public void TopByPrefix_IsEmpty_WhenNoPhrases()
         {
-            var phrases = new List<string>{"a"}.AsReadOnly();
+            var phrases = new List<string> {"a"}.AsReadOnly();
             var actualTopWords = AutocompleteTask.GetTopByPrefix(phrases, "b", 10);
             CollectionAssert.IsEmpty(actualTopWords);
         }
@@ -61,7 +82,7 @@ namespace Autocomplete
         [Test]
         public void CountByPrefix_IsTotalCount_WhenEmptyPrefix()
         {
-            var phrases = new List<string>{"a", "abc", "xyz"}.AsReadOnly();
+            var phrases = new List<string> {"a", "abc", "xyz"}.AsReadOnly();
             var actualCount = AutocompleteTask.GetCountByPrefix(phrases, "");
             var expectedCount = phrases.Count;
             Assert.AreEqual(expectedCount, actualCount);

85d53779d3d5e7d110791d3edd0887039d4a5fb2
Igor <51264207+DivideAndRule@users.noreply.github.com>
2019-11-23 20:32:48 +0500

    upd

--- js/substr/hash.js
+++ js/substr/hash.js
@@ -13,7 +13,7 @@ let showCollisions = false;
 let showTime = false;
 let count = 0;
 let keys = process.argv.slice(2, -3);
-let data;
+let result;
 for (let i = 0; i < keys.length; i++) {
     switch (keys[i]) {
         case "-c":
@@ -29,22 +29,21 @@ for (let i = 0; i < keys.length; i++) {
 }
 
 console.time('Elapsed time');
-
 switch (method) {
     case "b":
-        data = BruteForceFindSubstrIndices(str, substr);
+        result = BruteForceFindSubstrIndices(str, substr);
         console.log("Bruteforce method:");
         break;
     case "h1":
-        data = HashSumFindSubstrIndices(str, substr, 1);
+        result = HashFindSubstrIndices(str, substr, GetSumHash, 1);
         console.log("Hashsum method:");
         break;
     case "h2":
-        data = HashSumFindSubstrIndices(str, substr, 2);
+        result = HashFindSubstrIndices(str, substr, GetSumHash, 2);
         console.log("SquareHashsum method:");
         break;
     case "h3":
-        data = RabinKarpFindSubstrIndices(str, substr);
+        result = HashFindSubstrIndices(str, substr, CalcRabinKarpHash);
         console.log("RabinCarp mehod:");
         break;
     default:
@@ -52,10 +51,10 @@ switch (method) {
         return;
 }
 if (count > 0)
-    data.indices = data.indices.slice(0, count);
-console.log(data.indices.join());
+    result.indices = result.indices.slice(0, count);
+console.log(result.indices.join());
 if (showCollisions && method != "b")
-    console.log("Collisions:", data.collisions);
+    console.log("Collisions:", result.collisions);
 if (showTime)
     console.timeEnd('Elapsed time');
 
@@ -75,7 +74,7 @@ function BruteForceFindSubstrIndices(str, substr) {
     };
 }
 
-function GetHashSum(str, power) {
+function GetSumHash(str, power) {
     let hashSum = 0;
     for (let i = 0; i < str.length; i++) {
         hashSum += Math.pow(str[i].charCodeAt(), power);
@@ -83,13 +82,14 @@ function GetHashSum(str, power) {
     return hashSum;
 }
 
-function HashSumFindSubstrIndices(str, substr, power) {
+function HashFindSubstrIndices(str, substr, Method, power) {
     let indices = new Array();
-    let hashSumSubstr = GetHashSum(substr, power);
+    let substrHash = Method(substr, power);
+    let currentHash = Method(str.substr(0, substr.length), power)
     let collisions = 0;
     isNotSubstr:
     for (let i = 0; i < str.length - substr.length + 1; i++) {
-        if (GetHashSum(str.substr(i, substr.length), power) === hashSumSubstr) {
+        if (currentHash === substrHash) {
             for (let j = 0; j < substr.length; j++) {
                 if (str[i + j] != substr[j]) {
                     collisions += 1;
@@ -98,6 +98,10 @@ function HashSumFindSubstrIndices(str, substr, power) {
             }
             indices.push(i);
         }
+        currentHash =
+            Method === CalcRabinKarpHash
+                ? UpdRabinKarpHash(str, substr, currentHash, i)
+                : Method(str.substr(i + 1, substr.length), power);
     }
     return {
         indices,
@@ -113,28 +117,9 @@ function CalcRabinKarpHash(str) {
     return hash;
 }
 
-function RabinKarpFindSubstrIndices(str, substr) {
-    let indices = new Array();
-    let collisions = 0;
-    let substrHash = CalcRabinKarpHash(substr);
-    let currenttHash = CalcRabinKarpHash(str.substr(0, substr.length));
-    isNotSubstr:
-    for (let i = 0; i < str.length - substr.length + 1; i++) {
-        if (currenttHash == substrHash) {
-            for (let j = 0; j < substr.length; j++) {
-                if (str[i + j] != substr[j]) {
-                    collisions++;
-                    continue isNotSubstr;
-                }
-            }
-            indices.push(i);
-        }
-        if (i + substr.length <= str.length - 1)
-            currenttHash = 2 * (currenttHash - str[i].charCodeAt() * Math.pow(2, substr.length - 1))
-                + str[i + substr.length].charCodeAt();
-    }
-    return {
-        indices,
-        collisions
-    };
+function UpdRabinKarpHash(str, substr, currentHash, i) {
+    return i + substr.length === str.length
+        ? currentHash
+        : 2 * (currentHash - str[i].charCodeAt() * Math.pow(2, substr.length - 1))
+            + str[i + substr.length].charCodeAt();
 }

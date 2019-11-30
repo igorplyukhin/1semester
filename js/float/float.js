9cc8a7d7bb1726f516776690e7c6c402dd8c69d3
Igor Plyukhin <51264207+DivideAndRule@users.noreply.github.com>
2019-11-30 14:07:22 +0500

    11

--- js/float/float.js
+++ js/float/float.js
@@ -3,8 +3,7 @@ const cnvrts = require('./convert');
 const oprtns = require('./sum');
 
 let a = cnvrts.GetBinaryNumber('45672431');
-let b = cnvrts.
-GetBinaryNumber('45672431');
+let b = cnvrts.GetBinaryNumber('45672431');
 let c = oprtns.Add(a,b);
 console.log(c);
 let d = cnvrts.FloatParse(c);

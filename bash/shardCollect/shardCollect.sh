d3ad83755ae72bf574cff51891d88c1cdee89618
Igor Plyukhin <51264207+DivideAndRule@users.noreply.github.com>
2019-12-01 11:39:31 +0500

    Update shardCollect.sh

--- bash/shardCollect/shardCollect.sh
+++ bash/shardCollect/shardCollect.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-case "$1" in                                                                                                          
-        "-h"|"--help") 
+case "$1" in
+    "-h"|"--help")
         echo "Script shows fodlers without opened files"
         exit 0;;
-        "-d"|"--directory")
+    "-d"|"--directory")
         dir=$2;;
-        "")
-        dir=$(pwd);; 
+    "")
+        dir=$(pwd);;
 esac
 
 if ! [[ -d $dir ]]; then
@@ -17,22 +17,29 @@ fi
 openedFiles=$(lsof +D "$dir" | awk ' $1!="NAME " {print $9":"}')
 emptyFolderExist=0
 
-for folder in $(find "$dir" -maxdepth 1 -not -name "."  -type d)
+for folder in "$dir"/*
 do
-count=0
-    for file in $(find "$folder" -maxdepth 1 -type f)
+    count=0
+    if ! [[ -d $folder ]]; then
+        continue
+    fi
+
+    for file in "$folder"/*
     do
-        absFilePath=$(realpath "$file")
-        str=$(echo "$openedFiles" | grep "$absFilePath")
-        if [[ -n $str ]]; then 
+        if ! [[ -f $file ]]; then
+            continue
+        fi
+
+        if echo "$openedFiles" | grep -q "$file" 
+        then
             count=1
-            emptyFolderExist=1
         fi
     done
-    
-if [[ count -eq 0 ]]; then
-    echo "$(realpath "$folder") Doesn't have opened files"
-fi
+
+    if [[ count -eq 0 ]]; then
+        echo "$folder Doesn't have opened files"
+        emptyFolderExist=1
+    fi
 done
 
 if [[ emptyFolderExist -eq 0 ]]; then
